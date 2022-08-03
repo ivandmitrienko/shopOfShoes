fda34799b646974a531e373ace6bc8a2ac5121a9
ivandmitrienko <oneblackcrystal@gmail.com>
2022-08-03 22:07:53 +0300

    some changes

--- shop/src/App.jsx
+++ shop/src/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Routes, Route} from "react-router-dom";
 import Layout from './components/Layout';
 import style from'./App.module.css';
+import Homepage from './components/Homepage';
 
 export default class App extends Component {
   render() {
@@ -9,6 +10,11 @@ export default class App extends Component {
       <div className={style.App}>
         <Routes>
           <Route path="/" element={ <Layout /> }>
+            <Route path="/Collections" element={< Homepage />} />
+            <Route path="/Men" element={< Homepage />} />
+            <Route path="/Women" element={< Homepage />} />
+            <Route path="/About" element={< Homepage />} />
+            <Route path="/Contact" element={< Homepage />} />
           </Route>
         </Routes>
       </div>
--- shop/src/components/Layout.jsx
+++ shop/src/components/Layout.jsx
@@ -1,23 +1,30 @@
 import React, { PureComponent } from 'react';
-import style from './header.module.css'
+import { NavLink, Outlet} from "react-router-dom";
+import style from './Layout.module.css';
 
 export default class Layout extends PureComponent {
   render() {
     return (
-      <div className={style.header}>
-          <div className={style.header__menu}>
-            <div className={style.header__menu__left}>
-              <img src="imgs/sneakers 2.svg" alt="sneakers" />
-              <ul className={style.header__menu__left__links}>
-                <li>Collections</li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-              </ul>
+      <>
+        <div className={style.header}>
+            <div className={style.header__menu}>
+              <div className={style.header__menu__left}>
+                <img src="imgs/sneakers 2.svg" alt="sneakers" />
+                <div className={style.header__menu__left__links}>
+                  <NavLink to="/Collections">Collections</NavLink>
+                  <NavLink to="/Men">Men</NavLink>
+                  <NavLink to="/Women">Women</NavLink>
+                  <NavLink to="/About">About</NavLink>
+                  <NavLink to="/Contact">Contact</NavLink>
+                </div>
+              </div>
+              <div>
+                {/* <Basket /> */}
+              </div>
             </div>
-          </div>
-      </div>
+        </div>
+        <Outlet/>
+      </>
     )
   }
 }
