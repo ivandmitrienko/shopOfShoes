0cb17dd9f6efeb4719b30ce5a8d0b5e775716fe5
ivandmitrienko <oneblackcrystal@gmail.com>
2022-09-05 23:12:09 +0300

    initial commit

---
+++ shop/src/components/ImagesNav.jsx
@@ -0,0 +1,21 @@
+import React, { PureComponent } from 'react';
+import style from './ImagesNav.module.css';
+
+export default class ImagesNav extends PureComponent {
+
+    render() {
+        return (
+            <div className={style.slider__menu}>
+                {this.props.images.map((image, index) => {
+                    return <img
+                        src={image}
+                        className={this.props.activeImg === index ? style.imageActive : ''}
+                        alt=''
+                        key={image}
+                        onClick={() => this.props.setImage(index)} />
+                })
+                }
+            </div>
+        )
+    }
+}
--- shop/src/components/Slider.jsx
+++ shop/src/components/Slider.jsx
@@ -1,71 +1,37 @@
 import React, { PureComponent } from 'react';
 import { images } from './images.js';
-import { ReactComponent as Cross } from './icons/Combined Shape.svg';
-import { ReactComponent as Button } from './icons/Group 18.svg';
-import style from 'Slider.module.css';
+import Lightbox from './Lightbox.jsx';
+import ImagesNav from './ImagesNav.jsx';
+import style from './Slider.module.css';
 
 
 export default class Slider extends PureComponent {
 
-  constructor(props){
+  constructor(props) {
     super(props);
-
-    this.state= {
+    this.state = {
       activeImg: 0,
       lightBoxDisplay: false,
-      activeImgLightBox: 0,
     }
   }
 
-  setImage = (index) => { this.setState({ activeImg: index }) };
+  setImage = (index) => {this.setState({activeImg:index})};
 
   setLightBox = () => { this.setState({ lightBoxDisplay: !this.state.lightBoxDisplay }) };
 
-  LeftNextImage =  () => {
-    if (this.state.activeImg !== 0)
-    this.setState({ activeImg: this.state.activeImg - 1 });
-  }
-
-  RightNextImage = () => {
-    if (this.state.activeImg !== images.length - 1)
-      this.setState({ activeImg: this.state.activeImg + 1 });
-  }
-
   render() {
-    const setImage = <img src={images[this.state.activeImg]} alt='' />;
-    const inmagesNav = <div className={style.lightbox__menu}>
-      {images.map((image, index) => {
-        return <img
-          src={image}
-          className={this.state.activeImg === index ? style.imageActive : ''}
-          alt=''
-          key={image}
-          onClick={() => this.setImage(index)} />
-      })
-      }
-    </div>
-
-
     return (
-      <div className={style.lightbox}>
-        <div className={style.lightbox__img}>
+      <div className={style.slider}>
+        <div className={style.slider__img}>
           {<img src={images[this.state.activeImg]} alt='' onClick={this.setLightBox} />}
         </div>
-        {inmagesNav}
-        {this.state.lightBoxDisplay && <div className={style.lightBoxDisplay}>
-          <div className={style.showLightBox}>
-            <div className={style.setImage}>
-              <div className={style.cross}><Cross fill='#FFFFFF' onClick={this.setLightBox} /></div>
-              <div className={style.buttonOfLightbox}><Button className={style.stroke} onClick = {this.LeftNextImage}/></div>
-              <div className={style.buttonOfLightbox} style={{ left: '100%', transform: 'translate(-50%,-100%) rotate(0deg)' }}>
-                <Button className={style.stroke} onClick = {this.RightNextImage}/>
-              </div>
-              {setImage}
-            </div>
-            <div className={style.imagesNav}>{inmagesNav}</div>
-          </div>
-
-        </div>}
+        <ImagesNav images={images} activeImg={this.state.activeImg} setImage = {this.setImage}/>
+        {this.state.lightBoxDisplay &&
+          <Lightbox
+            images={images}
+            setLightBox={this.setLightBox}
+            activeImg={this.state.activeImg}
+          />}
       </div>
     )
   }
